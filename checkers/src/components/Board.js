27af0f7dee879f97c7a969564e34d8392137bd23
Ross Carter <rosscarter0423@gmail.com>
2020-09-07 20:46:49 -0400

    Further tried to render board

--- checkers/src/components/Board.js
+++ checkers/src/components/Board.js
@@ -3,84 +3,33 @@ import styles from './Board.css';
 
 export default class Board extends React.Component {
     render() {
+        const tiles = Array(64)
+            .fill(null)
+            .map((_, i) => <Tile row={i % 8} column={i / 8} key={i} />);
+        console.log(tiles);
+
         return (
-            <div className={styles.gameBoard}>
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-                <div className={styles.whiteTile} />
-                <div className={styles.blackTile} />
-            </div>
+            <div className={styles.gameBoard} children={tiles} />
         );
     }
 }
 
-/*class Tile extends React.Component {
-    render() {
-        if (this.props.color === styles.blackTile) {
-            return <div className={styles.blackTile} />
-        } else {
-            return <div className={styles.whiteTile} />
-        }
-    }
-}*/
+type Props = {
+    row: number,
+    column: number
+};
 
+class Tile extends React.Component<Props> {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        const { row, column } = this.props;
+
+        if (column * 8 + row % 2 === 0) {
+            return <div className={styles.blackTile} />;
+        }
+        return <div className={styles.whiteTile} />;
+    }
+}
